2094332216af2132c3cb3438857d26506eb07663
jinhee <jinhee@jinheeui-MacBookAir.local>
2025-09-20 11:25:38 +0900

    chore: 프로젝트 세팅

---
+++ apps/ui-docs/.storybook/preview.js
@@ -0,0 +1,17 @@
+import _ from '../node_modules/@jjinhee-olaf/ui/dist/styles/design-system.scss'
+
+console.log('_', _) //eslint-disable-line no-console
+
+/** @type { import('@storybook/react').Preview } */
+const preview = {
+    parameters: {
+        controls: {
+            matchers: {
+                color: /(background|color)$/i,
+                date: /Date$/i,
+            },
+        },
+    },
+}
+
+export default preview
